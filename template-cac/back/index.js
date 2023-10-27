8cdf79c36416ac6d3d30e4506f808d36d213aaae
Magdalena Arrua <magdalenarrua@gmail.com>
2023-10-27 20:27:25 -0300

    sequelize parte1

--- template-cac/back/index.js
+++ template-cac/back/index.js
@@ -1,6 +1,12 @@
 const server = require ("./src/server");
 const router = require("./src/routes");
-server.listen(3000, () =>{
-    console.log("Server listening on port 3000");
+const {conn} = require("./src/DB_connection");
 
+conn.sync({
+    force:true
+}).then(()=>{
+
+    server.listen(3000, () =>{
+        console.log("Server listening on port 3000");
+});
 });
